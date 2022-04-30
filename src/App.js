feb2b5c71478db2ccefb777ad6fc5dee3a6a2a5f
EverlastingMiracle <llachesis.mutas@gmail.com>
2022-04-30 11:42:20 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MintiForm from './components/MintiForm';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <MintiForm />
+    </div>
+  );
+}
+
+export default App;
